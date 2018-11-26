8a4089ddcb3265d8a4d6812cb4099bc93ce0713c
BT_1N3_27@BKTECH.LOCAL <BT_1N3_27@BKTECH.LOCAL>
2018-11-26 14:46:02 -0500

    Initial commit

---
+++ src/JackOLantern.java
@@ -0,0 +1,20 @@
+public class JackOLantern
+{
+    private String[][] faceFeatures;
+    public JackOLantern(String[][] faceFeatures)
+    {
+        this.faceFeatures=faceFeatures;
+    }
+    public void edit(String replace, int row, int column)
+    {
+
+    }
+    public void fill(String str)
+    {
+
+    }
+    public String toString()
+    {
+
+    }
+}
---
+++ src/JackOLanternRunner.java
@@ -0,0 +1,7 @@
+public class JackOLanternRunner
+{
+    String [][] face= new String[5][8];
+    JackOLantern face1= new JackOLantern(face);
+
+    face1.fill()
+}
